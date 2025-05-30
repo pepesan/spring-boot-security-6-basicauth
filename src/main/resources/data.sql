552f2a8e9792754aacafed4084d93fecb952b12f
David Vaquero <pepesan@gmail.com>
2025-05-30 14:15:42 +0200

    mejorado fichero de sql y de config

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,9 @@
 -- url encriptado: https://bcrypt-generator.com/
-INSERT INTO `USERS` VALUES
-                    (1,'pepesan@gmail.com','pepesan','{bcrypt}$2a$12$9w59VWZTWkgKqk1Z3VIzROFl17Go0QS6YsaaTDuqVLYymQ8kN5h3y','pepesan'),
+INSERT INTO `USERS` (`id`, `name`, `username`, `email`, `password`) VALUES
+  (1, 'pepesan', 'pepesan', 'pepesan@gmail.com', '{bcrypt}$2a$12$9w59VWZTWkgKqk1Z3VIzROFl17Go0QS6YsaaTDuqVLYymQ8kN5h3y'),
                     -- password: password
-                    (2,'admin@gmail.com','admin','{bcrypt}$2a$10$gqHrslMttQWSsDSVRTK1OehkkBiXsJ/a4z2OURU./dizwOQu5Lovu','admin');
-                    -- password: admin
-
+  (2, 'admin',   'admin',   'admin@gmail.com',   '{bcrypt}$2a$10$gqHrslMttQWSsDSVRTK1OehkkBiXsJ/a4z2OURU./dizwOQu5Lovu');
+  -- password: admin
 INSERT INTO `roles` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_USER');
 
 INSERT INTO `users_roles` VALUES (2,1),(1,2);
