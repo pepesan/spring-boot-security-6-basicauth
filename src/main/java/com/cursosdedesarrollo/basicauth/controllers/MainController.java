968f3c74e695a8f9d5e9ba487489f7048e8a2f65
David Vaquero <pepesan@gmail.com>
2025-05-30 13:14:52 +0200

    Initial commit

---
+++ src/main/java/com/cursosdedesarrollo/basicauth/controllers/MainController.java
@@ -0,0 +1,4 @@
+package com.cursosdedesarrollo.basicauth.controllers;
+
+
+
---
+++ src/test/java/com/cursosdedesarrollo/basicauth/BasicauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursosdedesarrollo.basicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
